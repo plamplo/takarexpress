bf06d03f03958c4c7756c1ff46c87e1d913b1845
batman1292 <batman12922@gmail.com>
2016-11-30 23:44:44 +0700

    kdk

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 var producer = require('./routes/producer');
 var mockdata = require('./routes/mockdata');
-var admin = require('./routes/admin');
 
 var app = express();
 
@@ -81,7 +80,65 @@ app.get('/product/search/:productID', mockdata.search); //search product data by
 app.get('/product/search_detail/:productID', mockdata.search_detail); //search product detail by product id
 app.get('/product/search_producer/:producerID', mockdata.search_producer); //search product data by producer id
 app.get('/product/search_detail_producer/:producerID', mockdata.search_detail_producer); //search product detail by producer id
-//admin login
-app.get('/admin', admin.login);
-app.get('/admin/index', admin.index);
+
 // module.exports = app;
+//admin login
+//app.get('/admin', admin.login);
+//app.get('/admin/index', admin.index);
+// module.exports = app;
+
+
+//////////////////////////
+app.engine('html', require('ejs').renderFile);
+
+app.use(session({secret: 'ssshhhhh'}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+var sess;
+
+app.get('/',function(req,res){
+sess = req.session;
+//Session set when user Request our app via URL
+if(sess.email) {
+/*
+* This line check Session existence.
+* If it existed will do some action.
+*/
+    res.redirect('/admin');
+}
+else {
+    res.render('admin/login');
+}
+});
+
+app.post('/login',function(req,res){
+  sess = req.session;
+//In this we are assigning email to sess.email variable.
+//email comes from HTML page.
+  sess.email=req.body.email;
+  res.end('done');
+});
+
+app.get('/admin',function(req,res){
+  sess = req.session;
+if(sess.email) {
+res.write('
+<h1>Hello '+sess.email+'</h1>
+');
+res.end('<a href="+">Logout</a>');
+} else {
+    res.write('
+     <h1>Please login first.</h1>
+    ');
+    res.end('<a href="+">Login</a>');
+}
+});
+
+app.get('/logout',function(req,res){
+req.session.destroy(function(err) {
+  if(err) {
+    console.log(err);
+  } else {
+    res.redirect('/');
+  }
+});
